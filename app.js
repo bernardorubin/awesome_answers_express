0b186b1daf5b4c24f052dc8087a3bd4cc8235101
bernulander <bernulander@gmail.com>
2017-03-06 13:31:42 -0800

    create project

---
+++ app.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+// 👇 body parser middleware can be used to parse json
+app.use(bodyParser.json());
+// 👇 body parser middleware can also be used to parse form data
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+// 👇 will transpile our scss files into css files
+app.use(require('node-sass-middleware')({
+  src: path.join(__dirname, 'public'),
+  dest: path.join(__dirname, 'public'),
+  indentedSyntax: true,
+  sourceMap: true
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// 👇 app.js is being exported as a model
+// we can't start this server by running the command 'node app.js'
+// the file that requires app.js would be the entry point to our node server
+module.exports = app;
